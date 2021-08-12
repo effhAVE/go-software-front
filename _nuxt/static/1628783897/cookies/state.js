389b487da8f3423e453fc4b7ca39a0e792a0a2f9
effhAVE <42100427+effhAVE@users.noreply.github.com>
2021-08-12 18:04:29 +0200

    Add files via upload

---
+++ _nuxt/static/1628783897/cookies/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fgo-software-front\u002F_nuxt\u002Fstatic\u002F1628783897",layout:"default",error:{statusCode:404,path:a,message:"This page could not be found"},serverRendered:false,routePath:a,config:{_app:{basePath:"\u002Fgo-software-front\u002F",assetsPath:"\u002Fgo-software-front\u002F_nuxt\u002F",cdnURL:null}}}}("\u002Fcookies"));
