042d2eab9528bd4e52b284b4f926309032a1b6f9
effhAVE <42100427+effhAVE@users.noreply.github.com>
2021-08-12 17:53:14 +0200

    Add files via upload

---
+++ _nuxt/static/1628783306/en/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FeffhAVE\u002F_nuxt\u002Fstatic\u002F1628783306",layout:"default",error:a,serverRendered:true,routePath:"\u002Fen",config:{_app:{basePath:"\u002FeffhAVE\u002F",assetsPath:"\u002FeffhAVE\u002F_nuxt\u002F",cdnURL:a}}}}(null));
